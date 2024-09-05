eac566e12b09ff513df28a093fca171db78495a7
spidy-naik <166608100+spidy-naik@users.noreply.github.com>
2024-09-05 16:10:21 +0530

    try old

--- Plugins/premium.py
+++ Plugins/premium.py
@@ -34,7 +34,7 @@ class PDF(FPDF):
         self.set_font('Arial', 'B', 16)
         self.set_text_color(0, 102, 204)  # Dark blue color
         self.cell(0, 10, 'Payment Receipt', 0, 1, 'C')
-        self.ln(20)  # Extra space after the title
+        self.ln(20)
 
     def footer(self):
         # Go to 1.5 cm from bottom
@@ -301,7 +301,7 @@ async def handle_utr_input(client, message):
                     pdf.cell(0, 10, 'Customer Information', 0, 1, 'L', 1)
                     pdf.set_font('Arial', '', 12)
                     pdf.set_fill_color(255, 255, 255)  # White background for text
-                    pdf.cell(0, 10, f'Customer Name: {username}', ln=True)
+                    pdf.cell(0, 10, f'Customer Name: {payer}', ln=True)
                     pdf.cell(0, 10, f'Telegram ID: {user_id}', ln=True)
                     pdf.ln(10)
 
@@ -310,7 +310,6 @@ async def handle_utr_input(client, message):
                     pdf.set_fill_color(204, 255, 204)  # Light green background
                     pdf.cell(0, 10, 'Transaction Details', 0, 1, 'L', 1)
                     pdf.set_font('Arial', '', 12)
-                    pdf.cell(0, 10, f'Payer Name: {payer}', ln=True)
                     pdf.cell(0, 10, f'Payment Amount: {amount}', ln=True)
                     pdf.cell(0, 10, f'Transaction ID: {utr}', ln=True)
                     pdf.cell(0, 10, f'Transaction Date: {current_time_ist.strftime("%Y-%m-%d %H:%M:%S IST")}', ln=True)
