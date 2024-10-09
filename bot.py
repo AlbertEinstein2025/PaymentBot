247f5f3b724eb0b6c9d184dcf969fc09aa2bfc6f
Silicon Developer <166461734+Silicon-Developer@users.noreply.github.com>
2024-10-09 19:57:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from config import *
 from pyrogram import Client 
 from datetime import datetime
+from aiohttp import web
 
 class Bot(Client):
     def __init__(self):
@@ -9,13 +10,18 @@ class Bot(Client):
             bot_token=BOT_TOKEN,
             api_id=API_ID,
             api_hash=API_HASH,
-            workers=20,
+            sleep_threshold=5,
+            workers=100,
             plugins={'root': 'Plugins'}
         )
 
     async def start(self):
         await super().start()
         me = await self.get_me()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
         self.uptime = datetime.now()
         print(f"New session started for {me.first_name}({me.username})")
 
